5ba77370fcc8f9576dd72af0a84ef0c7c235c37a
Patryk Janicki <kontakt.janicki@gmail.com>
2018-09-21 15:41:38 +0200

    Add css loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,20 @@ const { resolve } = require('path')
 
 module.exports = {
     entry: './src/index.js',
+
     mode: 'development',
+
     output: {
         filename: 'bundle.js',
         path: resolve(__dirname, 'dist')
     }, 
+
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                loader: 'css-loader'
+            }
+        ]
+    }
 }
