b45986956fffda96fdfc7fe1d3a4bab1b5002700
Patryk Janicki <kontakt.janicki@gmail.com>
2018-09-21 15:11:43 +0200

    Add mode and output to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const { resolve } = require('path')
+
 module.exports = {
-    entry: './src/index.js'
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist')
+    }, 
 }
