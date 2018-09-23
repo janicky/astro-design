30fa30d65ce0148fd73d8629665128e42bb73a5c
Patryk Janicki <kontakt.janicki@gmail.com>
2018-09-23 15:53:47 +0200

    Add parallax plugin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './resources/css/index.sass'
 import 'normalize.css'
+
+const Parallax = require('parallax-js')
+
+var scene = document.getElementById('parallax')
+var parallax = new Parallax(scene)
