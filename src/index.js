995d52290f7ba63c3c495bcc15acd0ea22b10600
Patryk Janicki <kontakt.janicki@gmail.com>
2018-09-23 20:02:08 +0200

    Add scroll-down button visibility on scroll

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ const Parallax = require('parallax-js')
 
 var scene = document.getElementById('parallax')
 var parallax = new Parallax(scene)
+
+var scroll_down = document.getElementById('scroll-down')
+document.addEventListener('scroll', () => {
+    if (window.scrollY > 250) {
+        scroll_down.classList.remove('jumbotron__scroll-down--active')
+    } else {
+        scroll_down.classList.add('jumbotron__scroll-down--active')
+    }
+})
