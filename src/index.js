0a0121a6d572b7dd61067e52f42184c93ef428b6
Patryk Janicki <kontakt.janicki@gmail.com>
2018-09-25 23:23:40 +0200

    Add header background on scroll

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ var parallax = new Parallax(scene)
 new Parallax(document.getElementById('moon-parallax'))
 
 var scroll_down = document.getElementById('scroll-down')
+var header = document.getElementsByClassName('header')[0]
+
 document.addEventListener('scroll', () => {
     if (window.scrollY > 250) {
         scroll_down.classList.remove('jumbotron__scroll-down--active')
+        header.classList.add('header--active')
     } else {
         scroll_down.classList.add('jumbotron__scroll-down--active')
+        header.classList.remove('header--active')
     }
 })
+
 
 $(function() {
     $('.slider__container').slick({
